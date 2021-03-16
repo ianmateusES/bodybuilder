c5c4be7d5719955e7059121f8e7d5155b46b40d6
ianmateusES <ec.ianmateus@alu.ufc.br>
2021-03-15 23:04:34 -0300

    Updating user data

--- backend/user-service/src/routes/profile.routes.js
+++ backend/user-service/src/routes/profile.routes.js
@@ -6,6 +6,33 @@ import ensureAuthenticated from '../app/middleware/ensureAuthenticated';
 const profileRouter = Router();
 
 profileRouter.use(ensureAuthenticated);
-profileRouter.get('/me', ProfileController.show);
+profileRouter.get('/', ProfileController.show);
+
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string(),
+      email: Joi.string().email(),
+      old_password: Joi.string(),
+      password: Joi.when('old_password', {
+        is: Joi.exist(),
+        then: Joi.required(),
+      }),
+      password_confirmation: Joi.when('password', {
+        is: Joi.exist(),
+        then: Joi.valid(Joi.ref('password')).required(),
+      }),
+      address: Joi.string(),
+      number: Joi.when('address', {
+        is: Joi.exist(),
+        then: Joi.number().required(),
+      }),
+      city: Joi.string(),
+      telephone: Joi.string().max(11).min(9),
+    },
+  }),
+  ProfileController.update,
+);
 
 export default profileRouter;
